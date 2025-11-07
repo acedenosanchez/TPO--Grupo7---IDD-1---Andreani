0defead87ee634d21534eae14af5f215208371c2
acedenosachez <acedenosanchez@uade.edu.ar>
2025-11-07 01:28:05 -0300

    Modificación en la creación de roles y users

--- 08_Roles.sql
+++ 08_Roles.sql
@@ -6,30 +6,42 @@ CREATE LOGIN operador01 WITH PASSWORD = 'Operador123!';
 CREATE USER operador01 FOR LOGIN operador01;
 GO
 
-
-
---Supervisor: revision de procesos y consultas analíticas
+-- Supervisor
 CREATE LOGIN supervisor01 WITH PASSWORD = 'Supervisor123!';
 CREATE USER supervisor01 FOR LOGIN supervisor01;
 GO
 
---Auditor: acceso de solo lectura y validación de trazabilidad
+-- Auditor
 CREATE LOGIN auditor01 WITH PASSWORD = 'Auditor123!';
 CREATE USER auditor01 FOR LOGIN auditor01;
 GO
 
-
---Permisos para el user operador
-GRANT INSERT, UPDATE ON SCHEMA::Logistica TO operador01;
-DENY DELETE ON SCHEMA::Logistica TO operador01;
+--Roles
+CREATE ROLE Rol_Operador;
+CREATE ROLE Rol_Supervisor;
+CREATE ROLE Rol_Auditor;
 GO
 
---Permisos para el user supervisor 
-GRANT SELECT, EXECUTE ON SCHEMA::Logistica TO supervisor01;
+--Asignación de permisos a cada Rol:
+
+--Operador
+GRANT INSERT, UPDATE ON SCHEMA::Logistica TO Rol_Operador;
+DENY DELETE ON SCHEMA::Logistica TO Rol_Operador;
 GO
 
---Permisos para el user auditor 
-GRANT SELECT ON SCHEMA::Logistica TO auditor01;
-DENY INSERT, UPDATE, DELETE ON SCHEMA::Logistica TO auditor01;
+--Supervisor
+GRANT SELECT, EXECUTE ON SCHEMA::Logistica TO Rol_Supervisor;
 GO
+
+--Auditor
+GRANT SELECT ON SCHEMA::Logistica TO Rol_Auditor;
+DENY INSERT, UPDATE, DELETE ON SCHEMA::Logistica TO Rol_Auditor;
+GO
+
+--Asignación de usuarios a Roles
+ALTER ROLE Rol_Operador ADD MEMBER operador01;
+ALTER ROLE Rol_Supervisor ADD MEMBER supervisor01;
+ALTER ROLE Rol_Auditor ADD MEMBER auditor01;
+GO
+
 
